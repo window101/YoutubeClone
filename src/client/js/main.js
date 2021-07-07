3076fff022c99d3acaafdb6a8c9a16a3bbc8b388
window101 <hwajun15@naver.com>
2021-07-07 21:02:40 +0900

    프론트앤드는 나중에...

---
+++ src/client/js/main.js
@@ -0,0 +1,7 @@
+//import fetch from "node-fetch";
+
+const hello = async () => {
+    alert("hi");
+    const x = await fetch("");
+};
+hello();
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+
+const path = require("path");
+
+
+module.exports = {
+    entry: "./src/client/js/main.js",
+    output: {
+        filename: "main.js",
+        
+        path: path.resolve(__dirname, "assets", "js"),
+    },
+};
+
+
